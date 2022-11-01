76b7e6174b9b373d2d4dd73211189f360e9b383e
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-11-01 11:25:30 +0900

    accounts-detail

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
+
+def detail(request, user_pk):
+    user = get_object_or_404(get_user_model(), pk=user_pk)
+    context = {
+        'user': user
+    }
+    return render(request, 'accounts/detail.html', context)
+

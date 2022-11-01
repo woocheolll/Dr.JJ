8bf5de560af7010e893f5c868c4698f712dce9a8
YoonDii <dbswl9506@naver.com>
2022-11-01 11:25:33 +0900

    articles CRUD

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ReviewForm
+from .models import Review
+
+# from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
+# from django.db.models import Q
 
 # Create your views here.
+
+
+def index(request):
+    reviews = Review.objects.order_by("-pk")
+    context = {"reviews": reviews}
+    return render(request, "articles/index.html", context)
+
+
+def create(request):
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, request.FILES)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect("articles:detail", review.pk)
+    else:
+        review_form = ReviewForm()
+    context = {"review_form": review_form}
+
+    return render(request, "articles/create.html", context)
+
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    comment_form = CommentForm()
+    context = {
+        "review": review,
+        "comment_form": comment_form,
+        "comments": review.comment_set.all(),
+    }
+
+    return render(request, "articles/detail.html", context)
+
+
+def update(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, request.FILES, instance=review)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("articles:detail", review.pk)
+    else:
+        review_form = ReviewForm(instance=review)
+    context = {"review_form": review_form}
+
+    return render(request, "articles/update.html", context)
+
+
+def delete(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    review.delete()
+    return redirect("articles:index")

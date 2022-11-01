98eba9088f9ddcd3497d7fa015d03dedb6176eab
YoonDii <dbswl9506@naver.com>
2022-11-01 11:54:29 +0900

    fixed

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm
-from django.contrib.auth import get_user_model,update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -14,7 +14,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect("accounts:signup")
+            return redirect("accounts:index")
     else:
         form = CustomUserCreationForm()
     context = {
@@ -22,18 +22,18 @@ def signup(request):
     }
     return render(request, "accounts/signup.html", context)
 
+
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('accounts:index')
+            return redirect("accounts:index")
     else:
         form = AuthenticationForm()
-    context = {
-        'form':form
-    }
-    return render(request, 'accounts/login.html',context)
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
 
 @login_required
 def logout(request):
@@ -49,12 +49,14 @@ def index(request):
     }
     return render(request, "accounts/index.html", context)
 
+
 @login_required
 def delete(request):
     request.user.delete()
     auth_logout(request)
     messages.warning(request, "탈퇴되었습니다.")
     return redirect("accounts:index")
+
 
 @login_required
 def changePassword(request):
@@ -71,19 +73,19 @@ def changePassword(request):
     }
     return render(request, "accounts/changepassword.html", context)
 
+
 def detail(request, user_pk):
     user = get_object_or_404(get_user_model(), pk=user_pk)
-    context = {
-        'user': user
-    }
+    context = {"user": user}
     return render(request, "accounts/detail.html", context)
+
 
 @login_required
 def follow(request, user_pk):
     user = get_user_model().objects.get(pk=user_pk)
     if request.user == user:
-        messages.warning(request, '스스로 팔로우 할 수 없습니다.')
-        return redirect('accounts:detail', user_pk)
+        messages.warning(request, "스스로 팔로우 할 수 없습니다.")
+        return redirect("accounts:detail", user_pk)
     if request.user in user.followers.all():
         user.followers.remove(request.user)
     else:

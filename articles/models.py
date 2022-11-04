1ed4140658bd73f502291e14b29ccd7d42359955
woocheolll <woocheol2868@gmail.com>
2022-11-04 14:42:58 +0900

    comment

--- articles/models.py
+++ articles/models.py
@@ -33,7 +33,6 @@ class Review(models.Model):
 
 class Comment(models.Model):
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
